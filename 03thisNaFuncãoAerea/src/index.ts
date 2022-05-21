bdc62291a3ee5a7581d507af7a74e7ed29acfb04
Tarcisio <caninosbrancos10031@gmail.com>
2022-05-21 19:22:05 -0300

    functionTHIS

---
+++ 03thisNaFuncãoAerea/src/index.ts
@@ -0,0 +1,11 @@
+// import './formulario';
+
+export function funcao(this: Date, nome: string, age: number): void {
+  console.log(this);
+  console.log(nome, age);
+}
+
+funcao.call(new Date(), 'Tar', 30);
+
+// coloca dentro de um array
+funcao.apply(new Date(), ['Tar', 30]);
